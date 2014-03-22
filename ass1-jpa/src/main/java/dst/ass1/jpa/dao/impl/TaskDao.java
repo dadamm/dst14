2c4f3387ff6a82fec56a6770e561c5ecaf064cf9
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-03-22 18:54:51 +0100

    create ExpertDao and impl methods from interface. two methods are not implemented.

---
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/TaskDao.java
@@ -0,0 +1,51 @@
+package dst.ass1.jpa.dao.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import dst.ass1.jpa.dao.ITaskDAO;
+import dst.ass1.jpa.model.ITask;
+import dst.ass1.jpa.model.impl.Task;
+
+public class TaskDao implements ITaskDAO {
+	
+	private Session session;
+	
+	public TaskDao(Session session) {
+		this.session = session;
+	}
+
+	@Override
+	public ITask findById(Long id) {
+		Transaction transaction = session.beginTransaction();
+		ITask task = (ITask) session.createCriteria(Task.class).add(Restrictions.eq("id", id)).uniqueResult();
+		transaction.commit();
+		return task;
+	}
+
+	@Override
+	public List<ITask> findAll() {
+		Transaction transaction = session.beginTransaction();
+		@SuppressWarnings("unchecked")
+		List<ITask> list = (List<ITask>) session.createCriteria(Task.class).list();
+		transaction.commit();
+		return list;
+	}
+
+	@Override
+	public List<ITask> findTasksForUserAndContext(String user, String c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ITask> findTasksForStatusFinishedStartandFinish(Date start, Date finish) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
