ca33c0cf248028224a4fa57f98cff6d3facb3627
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-03-24 07:17:16 +0100

    MetadataDao was implementing false interface.

--- ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/MetadataDao.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/MetadataDao.java
@@ -6,11 +6,11 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
-import dst.ass1.jpa.dao.IMembershipDAO;
-import dst.ass1.jpa.model.IMembership;
-import dst.ass1.jpa.model.impl.Membership;
+import dst.ass1.jpa.dao.IMetadataDAO;
+import dst.ass1.jpa.model.IMetadata;
+import dst.ass1.jpa.model.impl.Metadata;
 
-public class MetadataDao implements IMembershipDAO {
+public class MetadataDao implements IMetadataDAO {
 	
 	private Session session;
 	
@@ -19,18 +19,18 @@ public class MetadataDao implements IMembershipDAO {
 	}
 
 	@Override
-	public IMembership findById(Long id) {
+	public IMetadata findById(Long id) {
 		Transaction transaction = session.beginTransaction();
-		IMembership membership = (IMembership) session.createCriteria(Membership.class).add(Restrictions.eq("id", id)).uniqueResult();
+		IMetadata membership = (IMetadata) session.createCriteria(Metadata.class).add(Restrictions.eq("id", id)).uniqueResult();
 		transaction.commit();
 		return membership;
 	}
 
 	@Override
-	public List<IMembership> findAll() {
+	public List<IMetadata> findAll() {
 		Transaction transaction = session.beginTransaction();
 		@SuppressWarnings("unchecked")
-		List<IMembership> list = (List<IMembership>) session.createCriteria(Membership.class).list();
+		List<IMetadata> list = (List<IMetadata>) session.createCriteria(Metadata.class).list();
 		transaction.commit();
 		return list;
 	}
