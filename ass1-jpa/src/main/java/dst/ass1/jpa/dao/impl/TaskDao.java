9fae6ba10919441c3bddfca071aff15d1da19f88
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-04-01 13:32:55 +0200

    impl qbe in taskdao

--- ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/TaskDao.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/TaskDao.java
@@ -1,16 +1,22 @@
 package dst.ass1.jpa.dao.impl;
 
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dst.ass1.jpa.dao.ITaskDAO;
 import dst.ass1.jpa.model.ITask;
+import dst.ass1.jpa.model.ITaskProcessing;
+import dst.ass1.jpa.model.TaskStatus;
 import dst.ass1.jpa.model.impl.Task;
+import dst.ass1.jpa.model.impl.TaskProcessing;
 
 public class TaskDao implements ITaskDAO {
 	
@@ -38,14 +44,36 @@ public class TaskDao implements ITaskDAO {
 
 	@Override
 	public List<ITask> findTasksForUserAndContext(String user, String c) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.trace("call findTasksForUserAndContext for user {} and context {}", new Object[]{user, c});
+		Criteria criteria = session.createCriteria(Task.class);
+		if (user != null) {
+			criteria.createCriteria("user").add(Restrictions.eq("username", user));
+		}
+		if (c != null) {
+			criteria.createCriteria("metadata").add(Restrictions.eq("context", c));
+		}
+		@SuppressWarnings("unchecked")
+		List<ITask> list = criteria.list();
+		return list;
 	}
 
 	@Override
 	public List<ITask> findTasksForStatusFinishedStartandFinish(Date start, Date finish) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ITask> tasks = new LinkedList<ITask>();
+		
+		TaskProcessing taskProcessing = new TaskProcessing();
+		taskProcessing.setStatus(TaskStatus.FINISHED);
+		taskProcessing.setStart(start);
+		taskProcessing.setEnd(finish);
+		
+		Criteria criteria = session.createCriteria(TaskProcessing.class);
+		criteria.add(Example.create(taskProcessing));
+		@SuppressWarnings("unchecked")
+		List<ITaskProcessing> tasksProcessings = criteria.list();
+		for(ITaskProcessing p : tasksProcessings) {
+			tasks.add(p.getTask());
+		}
+		return tasks;
 	}
 
 }
