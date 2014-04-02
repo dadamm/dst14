44d7fa863790580b74815b54ac482a716b8faa23
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-04-03 00:38:57 +0200

    implement successfully MongoDbDataLoader.

---
+++ ass1-nosql/src/main/java/dst/ass1/nosql/impl/MongoDbDataLoader.java
@@ -0,0 +1,69 @@
+package dst.ass1.nosql.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.util.JSON;
+
+import dst.ass1.jpa.model.impl.Task;
+import dst.ass1.jpa.util.Constants;
+import dst.ass1.nosql.IMongoDbDataLoader;
+import dst.ass1.nosql.MongoTestData;
+
+public class MongoDbDataLoader implements IMongoDbDataLoader {
+	
+	private EntityManager em;
+	private MongoTestData testData;
+	
+	public MongoDbDataLoader(EntityManager em) {
+		this.em = em;
+		testData = new MongoTestData();
+	}
+	
+	private String createJsonString(Long taskid, Long lastUpdated) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("\"task_id\" : ");
+		sb.append(taskid);
+		sb.append(", ");
+		sb.append("\"last_updated\" : ");
+		sb.append(lastUpdated);
+		sb.append(", ");
+		sb.append("\"result_matrix\" : ");
+		sb.append(testData.getStringData(taskid.intValue()));
+		sb.append(", ");
+		sb.append("\"type\" : ");
+		sb.append("\"");
+		sb.append(testData.getDataDesc(taskid.intValue()));
+		sb.append("\"");
+		sb.append("}");
+		return sb.toString();
+	}
+	
+	@Override
+	public void loadData() throws Exception {
+		
+		@SuppressWarnings("unchecked")
+		List<Task> tasks = em.createNamedQuery("allFinishedTasks").getResultList();
+		
+		Mongo mongo = new Mongo();
+		DB db = mongo.getDB("dst");
+		DBCollection collection = db.getCollection(Constants.COLL_TASKRESULT);
+		
+		collection.createIndex(new BasicDBObject("task_id", 1));
+		
+		for(Task task : tasks) {
+			String jsonString = createJsonString(task.getId(), task.getTaskProcessing().getEnd().getTime());
+			collection.insert((DBObject) JSON.parse(jsonString));
+		}
+		
+		mongo.close();
+	}
+
+}
