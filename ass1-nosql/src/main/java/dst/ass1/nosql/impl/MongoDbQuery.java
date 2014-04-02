f6ab1d36c4db33172655d65720fa137d10fe6278
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-04-03 00:39:31 +0200

    impl first query of MongoDbQuery.

---
+++ ass1-nosql/src/main/java/dst/ass1/nosql/impl/MongoDbQuery.java
@@ -0,0 +1,48 @@
+package dst.ass1.nosql.impl;
+
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import dst.ass1.jpa.util.Constants;
+import dst.ass1.nosql.IMongoDbQuery;
+
+public class MongoDbQuery implements IMongoDbQuery {
+	
+	private DB db;
+	
+	public MongoDbQuery(DB db) {
+		this.db = db;
+	}
+
+	@Override
+	public Long findLastUpdatedByTaskId(Long id) {
+		DBCollection collection = db.getCollection(Constants.COLL_TASKRESULT);
+		BasicDBObject obj = new BasicDBObject("task_id", id);
+		
+		DBCursor cursor = collection.find(obj);
+		if(cursor.hasNext()) {
+			return (Long) cursor.next().get("last_updated");
+		} else {
+			System.out.println("cannot find task_id: " + id);
+			return -1L;
+		}
+	}
+
+	@Override
+	public List<Long> findLastUpdatedGt(Long time) {
+		
+		return null;
+	}
+
+	@Override
+	public List<DBObject> mapReduceWorkflow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
