d6fcd91d5112e799bc99195bcd5f3cea1eaf5bea
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-04-02 21:39:45 +0200

    implement sql interceptor.

--- ass1-jpa/src/main/java/dst/ass1/jpa/interceptor/SQLInterceptor.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/interceptor/SQLInterceptor.java
@@ -1,25 +1,28 @@
 package dst.ass1.jpa.interceptor;
 
+import java.util.regex.Pattern;
+
 import org.hibernate.EmptyInterceptor;
 
 public class SQLInterceptor extends EmptyInterceptor {
 
 	private static final long serialVersionUID = 3894614218727237142L;
+	
+	private static int selectCount;
 
-	public String onPrepareStatement(String sql) {
-		
-		// TODO
-		
+	public synchronized String onPrepareStatement(String sql) {
+		if(Pattern.matches("select [A-Za-z0-9,_. ]+ from .*(Expert|TaskForce).*", sql)) {
+			selectCount++;
+		}
 		return sql;
 	}
 
 	public static void resetCounter() {
-		// TODO
+		selectCount = 0;
 	}
 
 	
 	public static int getSelectCount() {
-		// TODO
-		return -1;
+		return selectCount;
 	}
 }
