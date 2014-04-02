9d591f9283b8ab165d321f0702ceb3a353498567
Thomas Petz <e0601280@student.tuwien.ac.at>
2014-04-02 21:22:05 +0200

    impl defaultlistener but time counting is missing.

--- ass1-jpa/src/main/java/dst/ass1/jpa/listener/DefaultListener.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/listener/DefaultListener.java
@@ -1,40 +1,72 @@
 package dst.ass1.jpa.listener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DefaultListener {
 	
-	// TODO
+	private static int loadOperations;
+	private static int updateOperations;
+	private static int removeOperations;
+	private static int persistOperations;
+	private static long overallTimeToPersist;
+	
+	private Map<Integer, Long> startTimes = new HashMap<Integer, Long>();
 
 	public static int getLoadOperations() {
-		// TODO
-		return -1;
+		return loadOperations;
 	}
 
 	public static int getUpdateOperations() {
-		// TODO
-		return -1;
+		return updateOperations;
 	}
 
 	public static int getRemoveOperations() {
-		// TODO
-		return -1;
+		return removeOperations;
 	}
 
 	public static int getPersistOperations() {
-		// TODO
-		return -1;
+		return persistOperations;
 	}
 
 	public static long getOverallTimeToPersist() {
-		// TODO
-		return -1;
+		return overallTimeToPersist;
 	}
 
 	public static double getAverageTimeToPersist() {
-		// TODO
-		return -1;
+		return (double) overallTimeToPersist / persistOperations;
 	}
 
 	public static void clear() {
-		// TODO
+		loadOperations = 0;
+		updateOperations = 0;
+		removeOperations = 0;
+		persistOperations = 0;
+		overallTimeToPersist = 0;
 	}
+	
+	// TODO add persist time information
+	public synchronized void onPrePersist(Object object) {
+//		startTimes.put(object.hashCode(), System.currentTimeMillis());
+	}
+	
+	// TODO add persist time information
+	public synchronized void onPostPersist(Object object) {
+//		overallTimeToPersist += System.currentTimeMillis() - startTimes.get(object.hashCode());
+		startTimes.remove(object.hashCode());
+		persistOperations++;
+	}
+	
+	public synchronized void onPostRemove(Object object) {
+		removeOperations++;
+	}
+	
+	public synchronized void onPostUpdate(Object object) {
+		updateOperations++;
+	}
+	
+	public synchronized void onPostLoad(Object object) {
+		loadOperations++;
+	}
+	
 }
