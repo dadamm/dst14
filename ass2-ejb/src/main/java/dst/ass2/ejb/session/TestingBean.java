e2a95a1b5f1d55ccccb92c28ca0a690b9968d9e8
Michael Voegler <m.voegler@infosys.tuwien.ac.at>
2014-04-03 17:47:10 +0200

    updated template for ass2

--- ass2-ejb/src/main/java/dst/ass2/ejb/session/TestingBean.java
+++ ass2-ejb/src/main/java/dst/ass2/ejb/session/TestingBean.java
@@ -12,19 +12,18 @@ import javax.persistence.EntityManager;
 
 import dst.ass1.jpa.model.IAddress;
 import dst.ass1.jpa.model.IExpert;
-import dst.ass1.jpa.model.ITaskForce;
-import dst.ass1.jpa.model.ITaskWorker;
-import dst.ass1.jpa.model.IMetadata;
-import dst.ass1.jpa.model.ITaskProcessing;
-import dst.ass1.jpa.model.IWorkPlatform;
-import dst.ass1.jpa.model.ITask;
 import dst.ass1.jpa.model.IMembership;
 import dst.ass1.jpa.model.IMembershipKey;
+import dst.ass1.jpa.model.IMetadata;
 import dst.ass1.jpa.model.IPerson;
+import dst.ass1.jpa.model.ITask;
+import dst.ass1.jpa.model.ITaskForce;
+import dst.ass1.jpa.model.ITaskProcessing;
+import dst.ass1.jpa.model.ITaskWorker;
 import dst.ass1.jpa.model.IUser;
-import dst.ass1.jpa.model.TaskStatus;
+import dst.ass1.jpa.model.IWorkPlatform;
 import dst.ass1.jpa.model.ModelFactory;
-import dst.ass1.jpa.util.test.TestData;
+import dst.ass1.jpa.model.TaskStatus;
 import dst.ass2.ejb.session.interfaces.ITestingBean;
 
 public class TestingBean implements ITestingBean {
@@ -33,7 +32,7 @@ public class TestingBean implements ITestingBean {
 
 	@Override
 	public void insertTestData() {
-		
+
 		ModelFactory modelFactory = new ModelFactory();
 
 		System.out.println("Started");
@@ -88,99 +87,99 @@ public class TestingBean implements ITestingBean {
 		em.persist(user2);
 		em.persist(expert);
 
-		IWorkPlatform ent1_1 = modelFactory.createPlatform();
-		ent1_1.setName(TestData.N_ENT1_1);
-		ent1_1.setLocation("location1");
-		ent1_1.setCostsPerWorkUnit(new BigDecimal(5));
+		IWorkPlatform platform1 = modelFactory.createPlatform();
+		platform1.setName("platform1");
+		platform1.setLocation("location1");
+		platform1.setCostsPerWorkUnit(new BigDecimal(5));
 
-		IWorkPlatform ent1_2 = modelFactory.createPlatform();
-		ent1_2.setName(TestData.N_ENT1_2);
-		ent1_2.setLocation("location2");
-		ent1_2.setCostsPerWorkUnit(new BigDecimal(7));
+		IWorkPlatform platform2 = modelFactory.createPlatform();
+		platform2.setName("platform2");
+		platform2.setLocation("location2");
+		platform2.setCostsPerWorkUnit(new BigDecimal(7));
 
-		em.persist(ent1_1);
-		em.persist(ent1_2);
+		em.persist(platform1);
+		em.persist(platform2);
 
-		ITaskForce ent2_1 = modelFactory.createTaskForce();
-		ent2_1.setName("tf1");
-		ent2_1.setLastMeeting(new Date());
-		ent2_1.setNextMeeting(new Date());
-		ent2_1.setExpert(expert);
-		ent2_1.setWorkPlatform(ent1_1);
+		ITaskForce tf1 = modelFactory.createTaskForce();
+		tf1.setName("cl1");
+		tf1.setLastMeeting(new Date());
+		tf1.setNextMeeting(new Date());
+		tf1.setExpert(expert);
+		tf1.setWorkPlatform(platform1);
 
-		ITaskForce ent2_2 = modelFactory.createTaskForce();
-		ent2_2.setName("tf2");
-		ent2_2.setLastMeeting(new Date());
-		ent2_2.setNextMeeting(new Date());
-		ent2_2.setExpert(expert);
-		ent2_2.setWorkPlatform(ent1_2);
+		ITaskForce tf2 = modelFactory.createTaskForce();
+		tf2.setName("cl2");
+		tf2.setLastMeeting(new Date());
+		tf2.setNextMeeting(new Date());
+		tf2.setExpert(expert);
+		tf2.setWorkPlatform(platform2);
 
-		expert.addAdvisedTaskForce(ent2_1);
-		expert.addAdvisedTaskForce(ent2_2);
+		expert.addAdvisedTaskForce(tf1);
+		expert.addAdvisedTaskForce(tf2);
 
-		ITaskWorker ent3_1 = modelFactory.createTaskWorker();
-		ent3_1.setName("longername1");
-		ent3_1.setWorkUnitCapacity(new Integer(4));
-		ent3_1.setLocation("AUT-XYZ@5678");
-		ent3_1.setJoinedDate(createDate(2000, 10, 10));
-		ent3_1.setLastTraining(createDate(2000, 10, 11));
-		ent3_1.setTaskForce(ent2_1);
+		ITaskWorker worker1 = modelFactory.createTaskWorker();
+		worker1.setName("longername1");
+		worker1.setWorkUnitCapacity(new Integer(4));
+		worker1.setLocation("AUT-XYZ@5678");
+		worker1.setJoinedDate(createDate(2000, 10, 10));
+		worker1.setLastTraining(createDate(2000, 10, 11));
+		worker1.setTaskForce(tf1);
 
-		ITaskWorker ent3_2 = modelFactory.createTaskWorker();
-		ent3_2.setName("longername2");
-		ent3_2.setWorkUnitCapacity(new Integer(6));
-		ent3_2.setLocation("AUT-XYZ@1234");
-		ent3_2.setJoinedDate(createDate(2000, 10, 12));
-		ent3_2.setLastTraining(createDate(2000, 10, 13));
-		ent3_2.setTaskForce(ent2_1);
+		ITaskWorker worker2 = modelFactory.createTaskWorker();
+		worker2.setName("longername2");
+		worker2.setWorkUnitCapacity(new Integer(6));
+		worker2.setLocation("AUT-XYZ@1234");
+		worker2.setJoinedDate(createDate(2000, 10, 12));
+		worker2.setLastTraining(createDate(2000, 10, 13));
+		worker2.setTaskForce(tf1);
 
-		ITaskWorker ent3_3 = modelFactory.createTaskWorker();
-		ent3_3.setName("longername3");
-		ent3_3.setWorkUnitCapacity(new Integer(8));
-		ent3_3.setLocation("AUT-XYZ@1234");
-		ent3_3.setJoinedDate(createDate(2000, 10, 14));
-		ent3_3.setLastTraining(createDate(2000, 10, 15));
-		ent3_3.setTaskForce(ent2_1);
+		ITaskWorker worker3 = modelFactory.createTaskWorker();
+		worker3.setName("longername3");
+		worker3.setWorkUnitCapacity(new Integer(8));
+		worker3.setLocation("AUT-XYZ@1234");
+		worker3.setJoinedDate(createDate(2000, 10, 14));
+		worker3.setLastTraining(createDate(2000, 10, 15));
+		worker3.setTaskForce(tf1);
 
-		ent2_1.addTaskWorker(ent3_1);
-		ent2_1.addTaskWorker(ent3_2);
-		ent2_1.addTaskWorker(ent3_3);
+		tf1.addTaskWorker(worker1);
+		tf1.addTaskWorker(worker2);
+		tf1.addTaskWorker(worker3);
 
-		ITaskWorker ent3_4 = modelFactory.createTaskWorker();
-		ent3_4.setName("longername4");
-		ent3_4.setWorkUnitCapacity(new Integer(4));
-		ent3_4.setLocation("AUT-XYZ@5678");
-		ent3_4.setJoinedDate(createDate(2000, 10, 16));
-		ent3_4.setLastTraining(createDate(2000, 10, 17));
-		ent3_4.setTaskForce(ent2_2);
+		ITaskWorker worker4 = modelFactory.createTaskWorker();
+		worker4.setName("longername4");
+		worker4.setWorkUnitCapacity(new Integer(4));
+		worker4.setLocation("AUT-XYZ@5678");
+		worker4.setJoinedDate(createDate(2000, 10, 16));
+		worker4.setLastTraining(createDate(2000, 10, 17));
+		worker4.setTaskForce(tf2);
 
-		ITaskWorker ent3_5 = modelFactory.createTaskWorker();
-		ent3_5.setName("longername5");
-		ent3_5.setWorkUnitCapacity(new Integer(8));
-		ent3_5.setLocation("AUT-XYZ@5678");
-		ent3_5.setJoinedDate(createDate(2000, 10, 18));
-		ent3_5.setLastTraining(createDate(2000, 10, 19));
-		ent3_5.setTaskForce(ent2_2);
+		ITaskWorker worker5 = modelFactory.createTaskWorker();
+		worker5.setName("longername5");
+		worker5.setWorkUnitCapacity(new Integer(8));
+		worker5.setLocation("AUT-XYZ@5678");
+		worker5.setJoinedDate(createDate(2000, 10, 18));
+		worker5.setLastTraining(createDate(2000, 10, 19));
+		worker5.setTaskForce(tf2);
 
-		ent2_2.addTaskWorker(ent3_4);
-		ent2_2.addTaskWorker(ent3_5);
+		tf2.addTaskWorker(worker4);
+		tf2.addTaskWorker(worker5);
 
-		em.persist(ent2_1);
-		em.persist(ent2_2);
+		em.persist(tf1);
+		em.persist(tf2);
 
-		em.persist(ent3_1);
-		em.persist(ent3_2);
-		em.persist(ent3_3);
-		em.persist(ent3_4);
-		em.persist(ent3_5);
+		em.persist(worker1);
+		em.persist(worker2);
+		em.persist(worker3);
+		em.persist(worker4);
+		em.persist(worker5);
 		em.persist(expert);
-		em.persist(ent2_2);
-		em.persist(ent1_1);
-		em.persist(ent1_2);
+		em.persist(tf2);
+		em.persist(platform1);
+		em.persist(platform2);
 
 		IMembership membership1 = modelFactory.createMembership();
 		IMembershipKey keyId1 = modelFactory.createMembershipKey();
-		keyId1.setWorkPlatform(ent1_1);
+		keyId1.setWorkPlatform(platform1);
 		keyId1.setUser(user1);
 		membership1.setRegistration(createDate(2009, 1, 1));
 		membership1.setDiscount(new Double(0.1));
@@ -188,7 +187,7 @@ public class TestingBean implements ITestingBean {
 
 		IMembership membership2 = modelFactory.createMembership();
 		IMembershipKey keyId2 = modelFactory.createMembershipKey();
-		keyId2.setWorkPlatform(ent1_1);
+		keyId2.setWorkPlatform(platform1);
 		keyId2.setUser(user2);
 		membership2.setRegistration(createDate(2008, 2, 2));
 		membership2.setDiscount(new Double(0.2));
@@ -196,7 +195,7 @@ public class TestingBean implements ITestingBean {
 
 		IMembership membership3 = modelFactory.createMembership();
 		IMembershipKey keyId3 = modelFactory.createMembershipKey();
-		keyId3.setWorkPlatform(ent1_2);
+		keyId3.setWorkPlatform(platform2);
 		keyId3.setUser(user1);
 		membership3.setRegistration(createDate(2007, 3, 3));
 		membership3.setDiscount(new Double(0.3));
@@ -206,33 +205,33 @@ public class TestingBean implements ITestingBean {
 		em.persist(membership2);
 		em.persist(membership3);
 
-		IMetadata meta1 = modelFactory.createMetadata();
-		meta1.setContext("workflow1");
-		meta1.setSettings(new ArrayList<String>());
+		IMetadata env1 = modelFactory.createMetadata();
+		env1.setContext("workflow1");
+		env1.setSettings(new ArrayList<String>());
 
-		IMetadata meta2 = modelFactory.createMetadata();
-		meta2.setContext("workflow2");
-		meta2.setSettings(new ArrayList<String>());
+		IMetadata env2 = modelFactory.createMetadata();
+		env2.setContext("workflow2");
+		env2.setSettings(new ArrayList<String>());
 
-		em.persist(meta1);
-		em.persist(meta2);
+		em.persist(env1);
+		em.persist(env2);
 
-		ITaskProcessing tp1 = modelFactory.createTaskProcessing();
-		tp1.setStart(new Date(System.currentTimeMillis() - 1800000));
-		tp1.setStatus(TaskStatus.SCHEDULED);
+		ITaskProcessing ex1 = modelFactory.createTaskProcessing();
+		ex1.setStart(new Date(System.currentTimeMillis() - 1800000));
+		ex1.setStatus(TaskStatus.SCHEDULED);
 
-		ITask t1 = modelFactory.createTask();
-		t1.setAssignedWorkUnits(new Integer(3));
-		t1.setProcessingTime(new Integer(0));
-		t1.setMetadata(meta1);
-		t1.setUser(user1);
-		t1.setTaskProcessing(tp1);
+		ITask task1 = modelFactory.createTask();
+		task1.setAssignedWorkUnits(new Integer(3));
+		task1.setProcessingTime(new Integer(0));
+		task1.setMetadata(env1);
+		task1.setUser(user1);
+		task1.setTaskProcessing(ex1);
 
-		tp1.setTask(t1);
-		tp1.addWorker(ent3_3);
-		ent3_3.addTaskProcessing(tp1);
-		em.persist(t1);
-		em.persist(ent3_3);
+		ex1.setTask(task1);
+		ex1.addWorker(worker3);
+		worker3.addTaskProcessing(ex1);
+		em.persist(task1);
+		em.persist(worker3);
 
 		System.out.println("Finished");
 
